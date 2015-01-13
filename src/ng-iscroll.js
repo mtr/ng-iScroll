2a8096a23d5aa86d7b10f18b966c7d9e2b768550
Martin Thorsen Ranang <mtr@ranang.org>
2015-01-13 11:03:30 +0100

    Merge with branch 'master' --strategy=ours

--- src/ng-iscroll.js
+++ src/ng-iscroll.js
@@ -1,34 +1,32 @@
 /*!
-Copyright (c) 2013 Brad Vernon <bradbury.vernon@gmail.com>
+ Copyright (c) 2013 Brad Vernon <bradbury.vernon@gmail.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
 
-*/
+ */
 
 
-angular.module('ng-iscroll', []).directive('ngIscroll', ['$timeout', function ($timeout)
-{
+angular.module('ng-iscroll', []).directive('ngIscroll', ['$timeout', function ($timeout) {
     return {
         replace: false,
         restrict: 'A',
-        link: function (scope, element, attr)
-        {
+        link: function (scope, element, attr) {
             // default timeout
             var ngiScroll_timeout = 5;
 
@@ -62,8 +60,7 @@ angular.module('ng-iscroll', []).directive('ngIscroll', ['$timeout', function ($
             }
 
             // iScroll initialize function
-            function setScroll()
-            {
+            function setScroll() {
                 if (scope.$parent.myScroll === undefined) {
                     scope.$parent.myScroll = [];
                 }
@@ -85,8 +82,7 @@ angular.module('ng-iscroll', []).directive('ngIscroll', ['$timeout', function ($
             }
 
             // watch for 'ng-iscroll' directive in html code
-            scope.$watch(attr.ngIscroll, function ()
-            {
+            scope.$watch(attr.ngIscroll, function () {
                 $timeout(setScroll, ngiScroll_timeout);
             });
 
